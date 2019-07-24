4e7727ba5c698fd6e2369b4202dc4169c2660409
b0wter <b0wter@gmx.de>
2019-07-24 20:41:08 +0200

    Dateiendungen in lower case umgewandelt.

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker stop lebesmart
 docker rm lebesmart
-GIT_SSH_COMMAND='ssh -i /home/b0wter/.ssh/id_rsa_lebesmart_deploy' git pull
+GIT_SSH_COMMAND='ssh -i /home/b0wter/.ssh/id_ed25519_gutsman_lebesmart_deployment' git pull
 docker build -t b0wter/lebesmart .
 docker run --name lebesmart --restart=always -d -p 8094:80 b0wter/lebesmart
